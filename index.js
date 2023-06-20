b3d8cd948663938d61f9a521e66224d547f4bd17
Vijay0806 <vijaym08062001@gmail.com>
2023-06-20 10:19:07 +0530

    second commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("THIS IS STACK OVER FLOW CLONE API");
+  res.send("This is a stack overflow clone API");
 });
 
 app.use("/user", useRoutes);
@@ -24,27 +24,11 @@ app.use("/answer", answerRoutes);
 const PORT = process.env.PORT || 5000;
 
 const DATABASE_URL = process.env.CONNECTION_URL;
-// mongoose.connect(dbUrl,{useNewUrlParser: true});
-
-const con=mongoose.connection;
-
-app.use(express.json());
- app.use(express.urlencoded({ extended:false }));
-
-try{
-  con.on('open',()=>{
-      console.log("MongoDB connected 😇 🥳 😍 !!!!")
-  });
-  }catch(error){
-    console.log("Error: " +error);
-  }   
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => {
-      console.log(`This Node server running on port ${PORT}`);
+      console.log(`server running on port ${PORT}`);
     })
   )
   .catch((err) => console.log(err.message));
-
-
-  // https://github.com/Vijay0806/Webcode-2-Backend
